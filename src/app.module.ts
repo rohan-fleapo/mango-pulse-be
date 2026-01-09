bbc59c5598a95522d7c8f495228ce121eeb6e8c8
Avilash Ghosh <97432966+Avilash-Fleapo@users.noreply.github.com>
2026-01-10 04:03:49 +0530

    minor fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,15 @@ import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards';
 import { MeetingsModule } from './meetings/meetings.module';
+import { PaymentsModule } from './payments/payments.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
 import { SupabaseModule } from './supabase/supabase.module';
 import { UsersModule } from './users/users.module';
 import { WhatsAppModule } from './whatsapp/whatsapp.module';
 import { ZoomModule } from './zoom/zoom.module';
-import { PaymentsModule } from './payments/payments.module';
 
 @Module({
   imports: [
-    // Global configuration module
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',

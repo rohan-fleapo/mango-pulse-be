6c3fac713745536a603daa98c183ed8304556fb7
fauzu-fleapo <fauzu@fleapo.com>
2026-01-10 02:05:08 +0530

    feat: add get engagement leaderboard endpoint

--- src/analytics/dto/index.ts
+++ src/analytics/dto/index.ts
@@ -1,6 +1,7 @@
 export * from './activity-analytics.dto';
 export * from './ai-insights.dto';
 export * from './analytics-query.dto';
+export * from './engagement-leaderboard.dto';
 export * from './meet-view-percentage-graph.dto';
 export * from './meeting-details-analytics.dto';
 export * from './meeting-stats.dto';
